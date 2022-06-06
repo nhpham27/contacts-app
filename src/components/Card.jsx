b55165c56cf8d82e9bab60685ed6a33dd6074c50
Nguyen Hoa Pham <nhpham27@gmail.com>
2022-06-06 15:14:05 -0500

    using map() to loop over contacts

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-
-
+import Avatar from "./Avatar";
+import Detail from "./Detail";
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img className="circle-img" src={props.img} alt="avatar_img" />
+        <Avatar img={props.img} />
       </div>
       <div className="bottom">
-        <p className="info">{props.phone}</p>
-        <p className="info">{props.email}</p>
+        <Detail info={props.phone} />
+        <Detail info={props.email} />
       </div>
     </div>
   );
